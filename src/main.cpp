d9875e896b9ac92c35ca478fe5e71a81eab6762b
cojode_inner <okanocan@Computer>
2024-02-01 13:35:33 +0300

    xxx

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,122 @@
-#include "lib/mtci_threads.h"
+#include <pthread.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+
 #include "util/mtci_transform.h"
+
+class MultiThreadConvert {
+ public:
+  uint8_t *data = NULL;
+  uint8_t *new_data = NULL;
+  size_t size = 0;
+  int w;
+  int h;
+  int channels;
+  size_t new_size = 0;
+  int new_w;
+  int new_h;
+  int new_channels;
+  uint8_t channel;
+  uint64_t center;
+  uint32_t cr;
+  uint32_t cc;
+  uint32_t ker_h;
+  uint32_t ker_w;
+  double *ker;
+
+  MultiThreadConvert() { size = 0; };
+  MultiThreadConvert(uint8_t *_data, size_t _size, int _w, int _h,
+                     int _channels, uint8_t _channel, uint64_t _center,
+                     uint32_t _cr, uint32_t _cc, uint32_t _ker_h,
+                     uint32_t _ker_w, double *_ker) {
+    memcpy(data, _data, _size);
+    new_data = new uint8_t[_w * _h];
+    memcpy(new_data, _data, _size);
+    w = _w;
+    h = _h;
+    channels = _channels;
+    channel = _channel;
+    center = _center;
+    cr = _cr;
+    cc = _cc;
+    ker_h = _ker_h;
+    ker_w = _ker_w;
+    ker = _ker;
+  }
+  ~MultiThreadConvert() {
+    delete[] new_data;
+    delete[] data;
+  }
+
+  MultiThreadConvert &mt_convolution(uint8_t channel, uint32_t ker_w,
+                                     uint32_t ker_h, double ker[], uint32_t cr,
+                                     uint32_t cc);
+};
+
+typedef struct {
+  uint64_t k;
+} data_t;
+
+extern MultiThreadConvert mtc;
+
+void *thread_convertion_task(void *ptr);
+
+#define BYTE_BOUND(value) value < 0 ? 0 : (value > 255 ? 255 : value)
+
+MultiThreadConvert &MultiThreadConvert::mt_convolution(
+    uint8_t channel, uint32_t ker_w, uint32_t ker_h, double ker[], uint32_t cr,
+    uint32_t cc) {
+  std::cout << w << "x" << h << std::endl;
+  // uint8_t* new_data = new uint8_t[w * h];
+  uint64_t center = cr * ker_w + cc;
+  pthread_t pool[50];
+  mtc = MultiThreadConvert(data, size, w, h, channels, channel, center, cr, cc,
+                           ker_h, ker_w, ker);
+
+  for (uint64_t k = channel; k < size; k += channels) {
+    for (int i = 0; i < 100; ++i) {
+      data_t data = {k};
+      pthread_create(&pool[i], NULL, thread_convertion_task, &data);
+    }
+  }
+  for (uint64_t k = channel; k < size; k += channels) {
+    data[k] = mtc.new_data[k / channels];
+  }
+  delete[] mtc.new_data;
+  delete[] mtc.data;
+  return *this;
+}
+
+void *thread_convertion_task(void *ptr) {
+  data_t *d = (data_t *)ptr;
+  uint64_t k = d->k;
+  double c = 0;
+  for (long i = -((long)mtc.cr); i < (long)mtc.ker_h - mtc.cr; ++i) {
+    long row = ((long)k / mtc.channels) / mtc.w - i;
+    if (row < 0) {
+      row = row % mtc.h + mtc.h;
+    } else if (row > mtc.h - 1) {
+      row %= mtc.h;
+    }
+    for (long j = -((long)mtc.cc); j < (long)mtc.ker_w - mtc.cc; ++j) {
+      long col = ((long)d->k / mtc.channels) % mtc.w - j;
+      if (col < 0) {
+        col = col % mtc.w + mtc.w;
+      } else if (col > mtc.w - 1) {
+        col %= mtc.w;
+      }
+      c += mtc.ker[mtc.center + i * (long)mtc.ker_w + j] *
+           mtc.data[(row * mtc.w + col) * mtc.channels + mtc.channel];
+    }
+  }
+  mtc.new_data[k / mtc.channels] = (uint8_t)BYTE_BOUND(round(c));
+  printf("%lf\n", c);
+}
 
 int main() {
   char path[] = "../materials/kisa.jpg";
